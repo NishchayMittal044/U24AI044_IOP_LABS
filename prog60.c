45d56027b0011f227dc00e4eed8b014e31b47267
NISHCHAY DEEPAK MITTAL <u24ai044@aid.svnit.ac.in>
2024-09-26 17:02:27 +0530

    Add files via upload

---
+++ prog60.c
@@ -0,0 +1,42 @@
+// PROGRAM TO SORT (SELECTION SORT) AN ARRAY
+#include <stdio.h>
+int main()
+{
+    int n, min_index;
+
+    printf("Enter the Number of Terms :");
+    scanf("%d", &n);
+
+    int a[n], b[n];
+
+    printf("Enter the Elements :");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[min_index] > a[j])
+            {
+                min_index = j;
+            }
+        }
+        if (min_index != i)
+        {
+            int temp = a[min_index];
+            a[min_index] = a[i];
+            a[i] = temp;
+        }
+    }
+
+    printf("The Sorted Array is :");
+    for (int z = 0; z < n; z++)
+    {
+        printf("%d ", a[z]);
+    }
+    return 0;
+}
