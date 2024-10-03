2f75c3439bfd7b15e4b52e8c1769a06901cd70f8
NISHCHAY DEEPAK MITTAL <u24ai044@aid.svnit.ac.in>
2024-10-03 16:53:09 +0530

    Update prog53.c

--- prog53.c
+++ prog53.c
@@ -7,31 +7,51 @@
 //   ***
 //    *
 
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    int n, i, k, a = 1;
-    n = 4;
-    a = n - 1;
-    for (k = 1; k <= n; k++)
+   
+    for (int i = 1; i <= 4; i++)
     {
-        for (i = 1; i <= a; i++)
-            printf(" ");
-        a--;
-        for (i = 1; i <= 2 * k - 1; i++)
+        for (int j = 4; j >= 1; j--)
+        {
+            if (j<=i)
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+            
+        }
+        for (int j = i-1; j >=1; j--)
+        {
             printf("*");
+        }
+        
         printf("\n");
     }
-    a = 1;
-    for (k = 1; k <= n - 1; k++)
+     for (int i = 3; i >= 1; i--)
     {
-        for (i = 1; i <= a; i++)
-            printf(" ");
-        a++;
-        for (i = 1; i <= 2 * (n - k) - 1; i++)
+        for (int j = 3; j >= 0; j--)
+        {
+            if (j<i)
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+            
+        }
+        for (int j = i-1; j >=1; j--)
+        {
             printf("*");
+        }
+        
         printf("\n");
     }
+
+
     return 0;
 }
