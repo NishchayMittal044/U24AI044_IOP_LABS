f6c156d0d9c30dad6a986586d849d8c07d28b09b
NISHCHAY DEEPAK MITTAL <u24ai044@aid.svnit.ac.in>
2024-10-06 22:54:45 +0530

    Update prog27.c

--- prog27.c
+++ prog27.c
@@ -5,7 +5,7 @@ int main()
     int num, flag = 0;
     printf("Enter the number you want to check ");
     scanf("%d", &num);
-    for (int i = 2; i < num; i++)
+    for (int i = 2; i < num/2; i++)
     {
         if (num % i == 0)
         {
