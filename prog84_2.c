25bfbeb0be4814ad43ef0dd13cec49ba9ee8b9b7
NISHCHAY DEEPAK MITTAL <u24ai044@aid.svnit.ac.in>
2024-11-24 03:03:24 +0530

    Add files via upload

---
+++ prog84_2.c
@@ -0,0 +1,42 @@
+// WRITE A FUNCTION TO READ INFORMATION OF BOOK AND DISPLAY THE INFORMATION
+// USING POINTER TO STRUCTURE.
+#include <stdio.h>
+
+struct Book
+{
+    char title[100];
+    char author[100];
+    int publicationYear;
+    float price;
+};
+
+void readBookInfo(struct Book *b)
+{
+    printf("Enter the book title: ");
+    scanf(" %s", &b->title);
+    printf("Enter the author name: ");
+    scanf(" %s", &b->author);
+    printf("Enter the publication year: ");
+    scanf("%d", &b->publicationYear);
+    printf("Enter the price of the book: ");
+    scanf("%f", &b->price);
+}
+
+void displayBookInfo(struct Book *b)
+{
+    printf("\nBook Information:\n");
+    printf("Title: %s\n", b->title);
+    printf("Author: %s\n", b->author);
+    printf("Publication Year: %d\n", b->publicationYear);
+    printf("Price: %f\n", b->price);
+}
+
+int main()
+{
+    struct Book book;
+
+    readBookInfo(&book);
+    displayBookInfo(&book);
+
+    return 0;
+}
